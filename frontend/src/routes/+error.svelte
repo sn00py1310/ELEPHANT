f4cb74f05d1b3d073b3dde54fc3e8ffdbd8322bd
Lucas Wiese <lucas.wiese@gmx.de>
2023-09-23 17:13:44 +0200

    error page

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div>
+	<h2>
+		{$page.status}
+	</h2>
+	<h1>Ein Fehler ist aufgetreten</h1>
+	<p>
+		{$page.error?.message}
+	</p>
+</div>
+
+<style>
+	div {
+		display: flex;
+		gap: 0.75rem;
+		color: var(--ctp-macchiato-red);
+		align-items: center;
+		flex-direction: column;
+		height: 100%;
+		width: 100%;
+		justify-content: center;
+		text-align: center;
+		padding: 1.25rem;
+	}
+
+	h2 {
+		font-size: 6rem;
+	}
+
+	h1 {
+		font-size: 1.5rem;
+		font-weight: bold;
+	}
+</style>
